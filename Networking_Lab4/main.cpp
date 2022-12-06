c3167f12fabd297462757de07150215d543dd9a8
Stelios Klapsis <Steliosklapsis@outlook.com>
2022-12-05 23:26:15 -0500

    Fixed Major 1

--- Networking_Lab4/main.cpp
+++ Networking_Lab4/main.cpp
@@ -17,11 +17,13 @@ int g_SessionID = 0;
 int main()
 {
 	void Handle_Get(http_request request);
+	void Handle_GameOver(http_request request);
 	void Handle_Post(http_request request);
 
 	http_listener listener(L"http://localhost:8777/SLCGame311");
 
 	listener.support(methods::GET, Handle_Get);
+	listener.support(methods::GET, Handle_GameOver);
 	listener.support(methods::POST, Handle_Post);
 
 	try
@@ -41,6 +43,37 @@ int main()
 	while (true);
 
 	return 0;
+}
+
+void Handle_GameOver(http_request request)
+{
+	cout << " get request made!" << endl;
+
+	wstring APIPath = request.absolute_uri().to_string();
+	wcout << "\nAPI PATH: " << APIPath << endl;
+
+	if (wcscmp(APIPath.c_str(), L"/SLCGame311/GameOver/") == 0)
+	{
+		if (request.headers().has(L"GameOver"))
+		{
+			value JSONObj = value::object();
+			http_headers reqHeaders = request.headers();
+
+			JSONObj[L"GameOver"] = value::string(reqHeaders[L"GameOver"]);
+
+			request.reply(status_codes::FailedDependency, JSONObj);
+		}
+		else
+		{
+			request.reply(status_codes::FailedDependency, "Failed Dependency");
+		}
+	}
+	else
+	{
+		request.reply(status_codes::BadRequest, "Bad Request");
+	}
+
+	request.reply(status_codes::OK, "Status OK");
 }
 
 void Handle_Get(http_request request)
@@ -122,3 +155,4 @@ void Handle_Post(http_request request)
 	}
 
 	request.reply(status_codes::OK, "Status OK");
+}
